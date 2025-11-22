ef4dd6f3c0d149d2830ab1023f6846bd74ec7e69
Oğuzhan Şekerci <oguzhansekerci14@gmail.com>
2025-11-22 10:14:12 +0300

    User-Post İlişkisi 09

--- hyperspace/services/urls.py
+++ hyperspace/services/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.service_list, name='service_list'),
+    path('<slug:slug>/', views.service_details, name='service_details'),  #slug yapılı bir slug göndericez 
 ]
--- hyperspace/services/views.py
+++ hyperspace/services/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Service
 
 # Create your views here.
@@ -6,3 +6,7 @@ from .models import Service
 def service_list(request):
     services = Service.objects.all()  ##sevice modelinden tüm servisleri alıyoruz
     return render(request, 'services/service_list.html', {'services': services})
+
+def service_details(request, slug):
+    service = get_object_or_404(Service, slug=slug)
+    return render(request, 'services/service_details.html', {'service': service})
